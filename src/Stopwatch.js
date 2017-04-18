3a10e9db4a936ffd8e27a8bdd865a614ccf22306
Sarah Goldgar <sgoldgar@gmail.com>
2017-04-18 18:38:52 -0500

    v1, not working

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(){
+    super();
+    this.state = {
+      seconds: 0
+    }
+  }
+  _startTime(){
+    let seconds = this.state.seconds;
+    let start = setInterval(seconds,1000);
+    this.setState({seconds: start});
+  }
+   _stopTime(){
+     let seconds = this.state.seconds;
+     let stop = clearInterval(seconds);
+     this.setState({seconds: stop});
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.seconds}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.setState({seconds:0})}>Reset</button>
+          <button onClick={()=>this._startTime.bind(this)}>Start</button>
+          <button onClick={()=>this._stopTime.bind(this)}>Pause</button>
         </div>
       </div>
     );
