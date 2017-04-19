382785ac1561eca629487cc2901863c3c3fce97b
Jon Franchi <jon.franchi@gmail.com>
2017-04-18 23:46:15 -0500

    Completed homework

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      time: 0
+    }
+  }
+
+  reset() {
+    this.setState({
+      time: 0
+    })
+  }
+
+  start () {
+    this.timer = setInterval(()=> {
+      this.setState({
+        time: this.state.time + 1
+      })
+    } , 1000);
+  }
+
+
+  pause () {
+    clearTimeout(this.timer)
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.time}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={()=>{this.reset()}}>Reset</button>
+          <button onClick={()=>{this.start()}}>Start</button>
+          <button onClick={()=>{this.pause()}}> Pause</button>
         </div>
       </div>
     );
@@ -17,3 +43,7 @@ class Stopwatch extends Component {
 }
 
 export default Stopwatch;
+
+//window.setTimeout(function, milliseconds);
+
+// window.clearTimeout(timeoutVariable)
