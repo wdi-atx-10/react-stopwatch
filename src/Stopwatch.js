ed9a400bec2084d13bd12e93cf61b3c91b8d9e4d
chris jauregui <chrisjay629@gmail.com>
2017-05-23 02:12:06 -0500

    chris

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
-import "./Stopwatch.css"
+import "./Stopwatch.css";
 
 class Stopwatch extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: 0,
+      paused: false,
+      intervalId: 0
+    }
+  }
+   Stopwatch(e) {
+  this.setState({ until: e.target.value });
+};
+
+handleStartClick() {
+  this.setState({ enabled: false });
+
+  this.interval = setInterval( () => {
+    this.tick();
+
+    if ( this.isTimeUp() ) {
+      this.finish();
+    }
+  }, 1000);
+};
+  startTime(){
+
+    this.setState({
+      paused: false
+    });
+    clearInterval(this.state.intervalId);
+    var intervalId = setInterval(() => {
+      if (this.state.paused===false){
+        this.setState({
+          time: this.state.time+1
+        });
+      }
+    
+    }, 1000);
+    this.setState({intervalId: intervalId});
+  }
+
+  resetTime(){
+    console.log("inside resetTime, this: ", this);
+    clearInterval(this.state.intervalId);
+    this.setState({
+      time: 0,
+      paused: true,
+      intervalId: 0
+    });
+    console.log("reset", this);
+  }
+
+  pauseTime(){
+    console.log("inside pauseTime, this: ", this);
+    clearInterval(this.state.intervalId);
+    this.setState({
+      paused: true,
+      intervalId: 0
+    });
+    console.log("reset", this);
+  }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1> {this.state.time} </h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.resetTime.bind(this)}>Reset</button>
+          <button onClick={this.startTime.bind(this)}>Start</button>
+          <button onClick={this.pauseTime.bind(this)}>Pause</button>
         </div>
       </div>
     );
